756c3786c96b5b705d85987212fd2db018c08d90
olivia <oliviakt23@gmail.com>
2020-09-06 01:27:38 -0500

    new script

---
+++ mydeletelog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Description: Logfiles delete Class Assignment
+#Author: Olivia K. Nkeih
+
+
+
+
+#find all logs that are older than 14days
+
+
+FILE= $(find /var/log/*.log -mtime +14)
+
+echo $FILE
+
+
